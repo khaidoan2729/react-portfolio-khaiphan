17d61487f515af4f423a104389e25f33fbec22ac
Khai Phan <khaidoanhtkg@gmail.com>
2024-01-14 22:14:19 -0800

    fixed contact info

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div className='container contact-page'>
             <div className='text-zone'>
-                
+                Page Under Maintenance. Please refer to LinkedIn for contact information.
             </div>
         </div>
     )
