9f59d8f9ba43b1b37759e22e222456656b8e39f8
Khai Phan <khaidoanhtkg@gmail.com>
2024-01-14 22:22:23 -0800

    fixed contact info

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div className='container contact-page'>
             <div className='text-zone'>
-                Page Under Maintenance. Please refer to LinkedIn for contact information.
+                Page Under Maintenance. Please refer to my <a href = "https://www.linkedin.com/in/khai-phan-b54757231/">LinkedIn</a> for contact information.
             </div>
         </div>
     )
